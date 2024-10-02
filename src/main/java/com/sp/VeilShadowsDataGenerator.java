abc287579aeb5f8d23c52ce3c11002560f6a863d
SpacePotato <rubywindowofficial1@gmail.com>
2024-10-02 10:15:47 -0400

    S H A D O W S

---
+++ src/main/java/com/sp/VeilShadowsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.sp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VeilShadowsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
